33db5bd411c2342a09d30db4f0643197ade7a738
Dog2657 <reeceya2006@gmail.com>
2025-01-24 23:14:44 +0000

    Added basic endpoint

--- FilerServer.go
+++ FilerServer.go
@@ -1,7 +1,17 @@
 package main
 
-import "log"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
-	log.Println("Hello World")
+	http.HandleFunc("/", handler)
+
+	log.Println("Listening...")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("%v", r.URL)
 }
